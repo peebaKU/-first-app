47178617b3d141d956ea9fbafea3217c7cf7c2a4
peebaKU <baramee.254450@gmail.com>
2024-02-26 02:31:47 +0700

    add delete function

--- app/page.js
+++ app/page.js
@@ -1,16 +1,54 @@
-import React  from 'react';
-import Allproducts from '@/components/Allproduct'
-import Show from '@/components/UseState';
+'use client'
+import {useState}  from 'react';
 
 const Home = () => {
-  const data = ['A', 'B', 'C', 'D']
-  return(
-    <div className='flex flex-col items-center justify-start min-h-screen'>
-      <div className='my-3 text-center w-2/3 text-3xl text-gray-600'>
-        All Product List 
+const productData = [
+  {id: '1', ProductName:'Kphone mini', price: 199},
+  {id: '2', ProductName:'Kphone Pro', price: 299},
+  {id: '3', ProductName:'Kphone Ultra', price: 399},
+  {id: '4', ProductName:'Kphone Pad', price: 379},
+  {id: '5', ProductName:'Kphone Pad Pro', price: 499},
+]
+  const [products, setProducts] = useState(productData)
+  
+  const deleteHandler = (id) => {
+    const index = products.findIndex(item => item.id === id);
+
+    if(index !== -1){
+      console.log(...products);
+      const updatedProducts = [...products];
+      updatedProducts.splice(index, 1);
+      setProducts(updatedProducts);
+    }
+  }
+
+  if(products.length === 0){
+    return(
+      <div className='flex flex-col items-center justify-start p-20'>
+              <div className='my-5 text-3xl text-gray-400'>
+              Empty Products
+              </div>
       </div>
-      <Allproducts/>
-      <Show></Show>
+    )
+  }
+  return(
+    <div className='flex flex-col items-center justify-start p-10'>
+      <div className='my-5 text-3xl text-gray-400'>Product Lists</div>
+      {
+        productData.map((item, index)=>{
+          const rowStyle = (index % 2 === 0)? 'bg-gray-200':'bg-gray-100'
+          return(
+            <div key={item.id} className={`flex justify-between w-7/12 p-3 ${rowStyle}`}>
+                <div>{item.ProductName}</div>
+                <button 
+                  onClick={deleteHandler.bind(this, item.id)} 
+                  className='py-1 px-2 ring-1 ring-gray-700 rounded-md hover:bg-gray-300 active:bg-red-500 active:text-white'>
+                  Delete
+                </button>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
